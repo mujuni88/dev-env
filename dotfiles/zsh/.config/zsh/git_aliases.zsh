6ceacf9f5ec05e5c4d06ca6603b1a744d3a5ad98
Joe Buza <jbuza@netflix.com>
2025-03-18 22:59:56 -0700

    feat: add git aliases for common git commands

---
+++ dotfiles/zsh/.config/zsh/git_aliases.zsh
@@ -0,0 +1,19 @@
+# Git Aliases
+alias gs='git status'
+alias ga='git add'
+alias gc='git commit'
+alias gcm='git commit -m'
+alias gco='git checkout'
+alias gb='git branch'
+alias gl='git log --oneline --graph --decorate'
+alias gp='git push'
+alias gpl='git pull'
+alias gd='git diff'
+
+# Additional aliases based on your common usage
+alias gcam='git cam'             # commit -am (as defined in your .gitconfig)
+alias gst='git st'               # status (as defined in your .gitconfig)
+alias gsts='git st -s'           # status short
+alias glol='git lol'             # log with graph (alias in your .gitconfig)
+alias grm='git rebasemain'  # pull origin main -r (as defined in your .gitconfig) 
+alias gstash='git staash'
