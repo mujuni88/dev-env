cb932ce2deabfa3198ddf82700739d81dcd29173
Joe Buza <jbuza@netflix.com>
2025-03-18 23:08:01 -0700

    chore: update git configuration and add new zsh aliases
    - Change the default branch in git configuration from 'master' to 'main'.
    - Update Neovim plugin commit hashes in lazy-lock.json for compatibility.
    - Add new zsh aliases for enhanced git command usage, including 'gundo', 'gpl', 'gcmd', and 'gcp'.

--- dotfiles/zsh/.config/zsh/git_aliases.zsh
+++ dotfiles/zsh/.config/zsh/git_aliases.zsh
@@ -11,9 +11,13 @@ alias gpl='git pull'
 alias gd='git diff'
 
 # Additional aliases based on your common usage
-alias gcam='git cam'             # commit -am (as defined in your .gitconfig)
-alias gst='git st'               # status (as defined in your .gitconfig)
-alias gsts='git st -s'           # status short
-alias glol='git lol'             # log with graph (alias in your .gitconfig)
-alias grm='git rebasemain'  # pull origin main -r (as defined in your .gitconfig) 
+alias gcam='git cam'
+alias gst='git st'
+alias gsts='git st -s'
+alias glol='git lol'
+alias grm='git rebasemain'
 alias gstash='git staash'
+alias gundo='git undo'
+alias gpl='git please'
+alias gcmd='git commadd'
+alias gcp='git cherry-pick'
