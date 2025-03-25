c35b7e69b72042e717278b4ddcbfc832eb2aa16e
Joe Buza <jbuza@netflix.com>
2025-03-25 16:00:52 -0700

    chore(zellij): update keybindings and remove unused files
    - Restore keybindings for pane movement in zellij configuration.
    - Replace Ctrl-h binding with Ctrl-m for switching to move mode.
    - Remove zellij.lua plugin configuration and zellij-autolock.wasm file as they are no longer needed.

--- dotfiles/nvim/.config/nvim/lua/jbuza/plugins/zellij.lua
+++
@@ -1,9 +0,0 @@
-return {
-	"https://github.com/fresh2dev/zellij.vim",
-	lazy = false,
-	init = function()
-		-- Options:
-		-- vim.g.zelli_navigator_move_focus_or_tab = 1
-		-- vim.g.zellij_navigator_no_default_mappings = 1
-	end,
-}
