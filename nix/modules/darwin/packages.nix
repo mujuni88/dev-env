730ef900c06f92a9bd91a3898722e5ce9e82a954
Joe Buza <jbuza@netflix.com>
2025-03-25 16:01:42 -0700

    chore(nvim): add KDL formatter to formatting settings and Nix packages
    - Include "kdlfmt" as a formatter for KDL in the Neovim formatting configuration.
    - Add "kdlfmt" to the Nix package list for KDL code formatting support.

--- nix/modules/darwin/packages.nix
+++ nix/modules/darwin/packages.nix
@@ -2,6 +2,7 @@
 with pkgs; [
   # Nix-specific tools
   alejandra # Nix code formatter
+  kdlfmt # KDL code formatter
 
   # Keep any other Nix-specific packages here that aren't available in Homebrew
 ]
