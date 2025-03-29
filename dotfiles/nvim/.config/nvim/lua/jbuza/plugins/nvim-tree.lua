7d9fe1bfeca1829bc1fe3aa81dfdf76de4359ffb
Joe Buza <jbuza@netflix.com>
2025-03-28 21:52:24 -0700

    fix: show file on explorer

--- dotfiles/nvim/.config/nvim/lua/jbuza/plugins/nvim-tree.lua
+++ dotfiles/nvim/.config/nvim/lua/jbuza/plugins/nvim-tree.lua
@@ -49,7 +49,7 @@ return {
 		local keymap = vim.keymap -- for conciseness
 
 		keymap.set("n", "<leader>ee", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle file explorer" })
-		keymap.set("n", "<leader>ef", "<cmd>NvimTreeFocus<CR>", { desc = "Focus file explorer" })
+		keymap.set("n", "<leader>ef", "<cmd>NvimTreeFindFileToggle<CR>", { desc = "Focus file explorer" })
 		keymap.set("n", "<leader>ec", "<cmd>NvimTreeCollapse<CR>", { desc = "Collapse file explorer" })
 		keymap.set("n", "<leader>er", "<cmd>NvimTreeRefresh<CR>", { desc = "Refresh file explorer" })
 
