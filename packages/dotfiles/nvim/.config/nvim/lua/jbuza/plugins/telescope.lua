e3f4f58f905bc776a43a4ecf24a54303749448a2
Joe Buza <jbuza@netflix.com>
2025-05-11 15:59:11 -0700

    chore: allow to grep hidden files

--- packages/dotfiles/nvim/.config/nvim/lua/jbuza/plugins/telescope.lua
+++ packages/dotfiles/nvim/.config/nvim/lua/jbuza/plugins/telescope.lua
@@ -36,7 +36,14 @@ return {
 				},
 			},
 			pickers = {
+				live_grep = {
+					file_ignore_patterns = { "node_modules", ".git", ".venv" },
+					additional_args = function(_)
+						return { "--hidden" }
+					end,
+				},
 				find_files = {
+					file_ignore_patterns = { "node_modules", ".git", ".venv" },
 					hidden = true,
 				},
 			},
