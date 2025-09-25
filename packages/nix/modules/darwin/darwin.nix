0623dc5d9b7da9960c1e4fb23a8604d5223f5036
Joe Buza <jbuza@netflix.com>
2025-09-24 20:34:06 -0700

    chore: update libs

--- packages/nix/modules/darwin/darwin.nix
+++ packages/nix/modules/darwin/darwin.nix
@@ -43,7 +43,7 @@ in {
           "/System/Applications/Messages.app"
           "/Users/jbuza/Applications/Slack.app"
           "/Users/jbuza/Applications/Arc.app"
-          "/System/Applications/Comet.app"
+          "/Applications/Comet.app"
           "/Users/jbuza/Applications/Notion.app"
           "/Users/jbuza/Applications/Cursor.app"
           "/Users/jbuza/Applications/Zed.app"
